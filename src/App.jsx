0e9ae8318b839a466e1a91dc3b4e21853bfc48f4
srysis <jashandenis@gmail.com>
2025-02-18 16:54:35 +0100

    imported a new 'product_page' page; imported 'react-router-dom' package to utilize routes for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react"
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Catalog from "./pages/catalog.jsx"
+import ProductPage from "./pages/product_page.jsx"
 
 function App() {
 	return (
-		<main>
-			<Catalog />
-		</main>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Catalog />} />
+				<Route path="/product/:id" element={<ProductPage />} />
+			</Routes>
+		</Router>
 	);
 }
 
