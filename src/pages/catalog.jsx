90e2e716323de3e188bff17dea70d47b82fed0b6
srysis <jashandenis@gmail.com>
2025-02-18 16:58:01 +0100

    switch from class component to a functional component for smaller file size; changed 'Product' component rendering tag attributes - now instead of passing fields of an object, an object itself is being passed

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 
 import products from "../files/products.json"
 
@@ -7,18 +6,12 @@ import "../style/catalog/catalog.scss"
 
 import Product from "../components/catalog/product.jsx"
 
-class Catalog extends React.Component {
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return(
-			<div id="catalog">
-				{products.map((product) => <Product key={product.id} boxArt={product.box_art} title={product.name} price={product.price} />)}
-			</div>
-		)
-	}
+function Catalog() {
+	return(
+		<div id="catalog">
+			{products.map((product) => <Product key={product.id} item={product} />)}
+		</div>
+	)
 }
 
 export default Catalog;
