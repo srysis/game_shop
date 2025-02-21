351d9e823167f7abd194ffb1e6ac617035b7e50b
srysis <jashandenis@gmail.com>
2025-02-21 13:33:08 +0100

    completely rewrote the logic for 'screenshot showcase' feature to be scalable and handle more than 3 images; removed some redundant variables; renamed variables to use 'snake case' and functions to use 'camel case' types of writing

--- src/components/product_page/product_screenshots.jsx
+++ src/components/product_page/product_screenshots.jsx
@@ -1,95 +1,136 @@
 import React from 'react';
 
-import Image from "./Image.jsx"
+import Screenshot from "./screenshot.jsx"
 
-function ProductScreenshots( {image_list} ) {
+
+// !! STARTING INDEX IS CONSIDERED AS 1 IN THIS COMPONENT
+
+function ProductScreenshots( {image_list, toggleFullscreenFunction, setCurrentImageFunction, setCurrentImageIndexFunction} ) {
 	// a state that is used to force a component re-render
 	// for some reason React doesn't call a re-render after state change, so this workaround was implemented
 	const [, forceUpdate] = React.useReducer(x => x + 1, 0);
 
 	const image_paths = Object.values(image_list);
 
-	// create a clone of initial array of images paths and store it into a temporary array for modification
-	// it was done because attempting to simply assing an existing to an empty one to create a copy, modifying a copy
-	// will also change the original object, hence why it is required to make a clone and not a copy
+	// create a clone of initial array of images paths and store it into a temporary array for modifications
+	// it was done because attempting to simply assign an existing array to an empty one to create a copy, 
+	// modifying a copy would also change the original array, hence why it is required to make a clone and not a copy
 	let cloned_image_array = structuredClone(image_paths);
 
-	// changing a length of array will remove all elements index of which is bigger than "given length value + 1"
+	// remove all elements for new array after a certain index (3, in this case)
 	cloned_image_array.length = 3;
 
-	const [current_images, set_current_images] = React.useState(cloned_image_array);
+	// 'current_images' array will store paths that are currently rendered
+	// it's elements will change after every call of 'goToLeft()' or 'goToRight()' functions
+	// initially, it stores first 3 paths of 'image_list', so that only they are shown on a page
+	const [current_images, setCurrentImages] = React.useState(cloned_image_array);
 
-	// this variable will store an array with paths to images that should be rendered on page
-	let images_to_render = current_images;
 
-	// since there are only 3 images that need to be rendered,
-	// set the last image index as 3, so switching will work from the 3rd image
-	const [image_index, set_image_index] = React.useState(3);
+	// to properly define which images should render next and which shouldn't,
+	// we need to keep track of which image is currently on the left end, and which is on the right end
+	const [image_index_to_left, setImageIndexToLeft] = React.useState(1);
+	const [image_index_to_right, setImageIndexToRight] = React.useState(cloned_image_array.length)
+
+
+	// goToLeft() and goToRight() functions will calculate index values that should come next
+	// so they will pull out the required path from 'image_paths' array and put it into a 'current_images' array
 
 	function goToLeft() {
-		// to determine which image will be next in order, this variable will be used
-		// to store the value of potential index that will be used to pull the right path
-		// from list of image paths
-		let current_image_index = image_index;
-
-		// make a copy of array in a state to avoid modifying states directly
 		let result_array = current_images;
 
 		result_array.pop();
+		
+		// these vars will be used as a temp storage for next index value for each end
+		let next_image_index_to_left = image_index_to_left;
+		let next_image_index_to_right = image_index_to_right;
 
-		// decrement potential index value by 1
-		current_image_index--;
+		// subtract 1 from each index value
+		next_image_index_to_left--;
+		next_image_index_to_right--;
 
-		// check potential index value to decide whether the index should be reset or not
-		// reset if index value becomes negative, do not reset if it does not
-		if (current_image_index < 0) {
-			current_image_index = image_paths.length - 1;
-			set_image_index(current_image_index);
-		} else {
-			set_image_index(current_image_index);
+		// check if index value for left end will become less than 1
+		// if it will - reset it to the length value of array that stores paths to images
+		// aka last element index
+		if (next_image_index_to_left < 1) {
+			next_image_index_to_left = image_paths.length;
 		}
 
-		result_array.unshift(image_paths[image_index]);
-		set_current_images(result_array);
+		// same method for index value for right end
+		if (next_image_index_to_right < 1) {
+			next_image_index_to_right = image_paths.length;
+		}
 
+		// update both index values in states
+		setImageIndexToLeft(next_image_index_to_left);
+		setImageIndexToRight(next_image_index_to_right);
+
+		// pull the string, positioned at required index, out of the array 
+		// and put it at the beginning of the array that will be assigned to state array
+		result_array.unshift(image_paths[next_image_index_to_left - 1]);
+
+		// update array that stores paths to images that should be rendered
+		setCurrentImages(result_array);
+
+		// force re-render in order to render new images
 		forceUpdate();
 	}
 
 	function goToRight() {
-		// to determine which image will be previous in order, this variable will be used
-		// to store the value of potential index that will be used to pull the right path
-		// from list of image paths
-		let current_image_index = image_index;
-
-		// make a copy of array in a state to avoid modifying states directly
 		let result_array = current_images;
 
 		result_array.shift();
+		
+		// these vars will be used as a temp storage for next index value for each end
+		let next_image_index_to_left = image_index_to_left;
+		let next_image_index_to_right = image_index_to_right;
 
-		// increment potential index value by 1
-		current_image_index++;
+		// add 1 from each index value
+		next_image_index_to_left++;
+		next_image_index_to_right++;
 
-		// check potential index value to decide whether the index should be reset or not
-		// reset if index value goes out of bounds, do not reset if it does not
-		if (current_image_index > image_paths.length - 1) {
-			current_image_index = 0;
-			set_image_index(current_image_index);
-		} else {
-			set_image_index(current_image_index);
+
+		// check if index value for left end will go out of bounds
+		// if it will - reset it to 1, aka first element index
+		if (next_image_index_to_left > image_paths.length) {
+			next_image_index_to_left = 1;
 		}
 
-		result_array.push(image_paths[image_index]);
-		set_current_images(result_array);
+		// same method for index value for right end
+		if (next_image_index_to_right > image_paths.length) {
+			next_image_index_to_right = 1;
+		}
 
+		// update both index values in states
+		setImageIndexToLeft(next_image_index_to_left);
+		setImageIndexToRight(next_image_index_to_right);
+
+
+		// pull the string, positioned at required index, out of the array 
+		// and put it at the end of the array that will be assigned to state array
+		result_array.push(image_paths[next_image_index_to_right - 1]);
+
+		// update array that stores paths to images that should be rendered
+		setCurrentImages(result_array);
+
+		// force re-render in order to render new images
 		forceUpdate();
 	}
 
 	return(
 		<div className="product_screenshots">
 			<div className="left_button" onClick={ () => { goToLeft(); } } >&lt;</div>
-				{images_to_render.map((image, index) => 
+				{current_images.map((image, index) => 
 					{
-						return <Image key={index + 1} image_src={image} />
+						return <Screenshot 
+									key={index + 1} 
+									image_src={image} 
+									onClickHandler={ () => { 
+											toggleFullscreenFunction(true); 
+											setCurrentImageFunction(image);  
+											console.log('toggle on'); 
+										} 
+									} 
+								/>
 					}
 				)}
 			<div className="right_button" onClick={ () => { goToRight(); } } >&gt;</div>
