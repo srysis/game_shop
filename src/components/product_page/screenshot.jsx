38b8b467750c0ca4b5ca930da7bef69c9fce9497
srysis <jashandenis@gmail.com>
2025-02-24 10:57:28 +0100

    added a 'data_index' prop to be set as 'data-index' attribute

--- src/components/product_page/screenshot.jsx
+++ src/components/product_page/screenshot.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Screenshot( { image_src, onClickHandler } ) {
+function Screenshot( { image_src, data_index, onClickHandler } ) {
 
 	return(
 		<div className="image_container">
-			<img src={`media/images/screenshots/${image_src}`} onClick={() => onClickHandler()} />
+			<img src={`media/images/screenshots/${image_src}`} data-index={data_index} onClick={() => onClickHandler()} />
 		</div>
 	)
 }
