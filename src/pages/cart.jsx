e1ee2c707a4b649369862398b34a231d307c1bfa
srysis <jashandenis@gmail.com>
2025-02-25 23:16:14 +0100

    added a 'cart' page

---
+++ src/pages/cart.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+import CartProduct from "../components/cart/cart_product.jsx"
+
+import "../style/cart/cart.scss"
+
+function Cart( {products_in_cart, removeFunction} ) {
+	if (products_in_cart.length > 0) {
+		return (
+			<div id="cart">
+				{products_in_cart.map((item) => {
+					return <CartProduct key={item.id} product={item} onRemove={removeFunction} />
+				})}
+			</div>
+		)
+	} else {
+		return (
+			<div id="cart">
+				<h2>Cart is empty</h2>
+			</div>
+		)
+	}
+
+	
+}
+
+export default Cart;
