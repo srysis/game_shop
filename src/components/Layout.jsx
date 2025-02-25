a06b5fb81729244171a396571cfa98de31b64fdb
srysis <jashandenis@gmail.com>
2025-02-25 23:07:48 +0100

    added a 'layout' for the entire app

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import { Outlet } from 'react-router-dom'
+
+import Header from "./Header.jsx"
+
+
+function Layout( ) {
+	return (
+		<>
+			<Header />
+			<Outlet />
+			
+		</>
+	);
+}
+
+export default Layout;
