61caa8c3a206b1b41f8f82a7ded92c561fcd2a5e
srysis <jashandenis@gmail.com>
2025-02-27 15:35:26 +0100

    new imports; added conditional rendering for on-screen messages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,14 +3,25 @@ import React from "react"
 import { Outlet } from 'react-router-dom'
 
 import Header from "./Header.jsx"
+import SuccessAddMessage from "./SuccessMessage.jsx"
+import SuccessRemoveMessage from "./SuccessMessage.jsx"
 
 
-function Layout( ) {
+function Layout( {was_added, was_removed} ) {
+	let message_type = "";
+
+	if (was_added) { 
+		message_type = "add";
+	} else if (was_removed) { 
+		message_type = "remove";
+	}
+
 	return (
 		<>
+			{ was_added && <SuccessAddMessage type={message_type} /> }
+			{ was_removed && <SuccessRemoveMessage type={message_type} /> }
 			<Header />
 			<Outlet />
-			
 		</>
 	);
 }
