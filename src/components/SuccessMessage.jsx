71c355e1ce202e2bd15ac94f2435cd315ade76b6
srysis <jashandenis@gmail.com>
2025-02-27 15:36:17 +0100

    added a component that renders on-screen messages

---
+++ src/components/SuccessMessage.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import acknowledge_icon from "../files/confirm-icon.png"
+
+import "../style/layout/message.scss"
+
+function SuccessMessage( {type} ) {
+	return (
+		<div className={type === "add" ? "message added" : type === "remove" ? "message removed" : ""} >
+			<div className="icon_container">
+				<img src={acknowledge_icon} />
+			</div>
+			<div className="text_container">
+				<h2>Success!</h2>
+				<p>{type === "add" ? "Product was added to your cart" : type === "remove" ? "Product was removed from your cart" : undefined}</p>
+			</div>
+		</div>
+	);
+}
+
+export default SuccessMessage;
