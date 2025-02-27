49bd6146bccf51599e6d1054d054b83b13b426ff
srysis <jashandenis@gmail.com>
2025-02-27 15:42:41 +0100

    wrapped 'cart_product' container's content in the link - now user can click on an item within the cart and they will be redirected to the corresponding product's page

--- src/components/cart/cart_product.jsx
+++ src/components/cart/cart_product.jsx
@@ -1,15 +1,25 @@
 import React from "react"
+import { Link } from "react-router";
 
 import remove_icon from "../../files/cross.svg"
 
 function CartProduct( {product, onRemove} ) {
 	return (
 		<div className="cart_product">
-			<div className="product_info">
-				<h1>{product.name}</h1>
-			</div>
-			<div className="remove_from_cart_container">
-				<button type="button" className="remove_from_cart" onClick={ () => { onRemove(product.id) } }><img src={remove_icon} /></button>
+			<div className="link_container">
+				<div className="overlay">
+					<Link to={`/product/${product.id}`} />
+				</div>
+				<div className="box_art">
+					<img src={`media/images/box_art/${product.box_art}`} alt={`${product.name} box art`} />
+				</div>
+				<div className="product_info">
+					<h2><Link to={`/product/${product.id}`}>{product.name}</Link></h2>
+					<p>{product.price}</p>
+				</div>
+				<div className="remove_from_cart_container">
+					<button type="button" className="remove_from_cart" onClick={ () => { onRemove(product.id) } }><img src={remove_icon} /></button>
+				</div>
 			</div>
 			
 		</div>
