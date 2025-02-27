843f38f74294680e954af39ac03f21a5294ac727
srysis <jashandenis@gmail.com>
2025-02-27 15:34:14 +0100

    implemented displaying messages depending on the user's actions(adding to or removing product from the cart); removed some 'console.logs'

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,61 @@ import Cart from "./pages/cart.jsx"
 function App() {
 	const [cart_content, setCartContent] = React.useState([]);
 
+	const [was_added, setAdded] = React.useState(false);
+	const [was_removed, setRemoved] = React.useState(false);
+
+
+	function showMessage(type) {
+		if (type === "add") {
+			setAdded(true);
+
+			toggleMessage(type);
+
+		} else if (type === "remove") {
+			setRemoved(true);
+
+			toggleMessage(type);
+		}
+	}
+
+	function toggleMessage(type) {
+		if (type === "add") {
+			setTimeout(setAddedMessageTimeout, 5000);
+		} else if (type === "remove") {
+			setTimeout(setRemovedMessageTimeout, 5000);
+		}
+	}
+
+	function setAddedMessageTimeout() {
+		setAdded(false);
+	}
+
+	function setRemovedMessageTimeout() {
+		setRemoved(false);
+	}
+
 	function canBeAddedToCart(product) {
 		return (cart_content.includes(product));
 	}
 
 	function addToCart(product) {
-		console.log("Received: " + product);
-
 		setCartContent([...cart_content, product]);
+
+		showMessage("add");
 	}
 
 	function removeFromCart(product_id) {
 		const new_cart_content = cart_content.filter((item) => item.id !== product_id);
 
 		setCartContent(new_cart_content);
+
+		showMessage("remove");
 	}
 
 	return (
 		<Router>
 			<Routes>
-				<Route element={<Layout />} >
+				<Route element={<Layout was_added={was_added} was_removed={was_removed} />} >
 					<Route path="/" element={<Home />} />
 					<Route path="/product/:id" element={<ProductPage addToCartFunction={addToCart} isDuplicate={canBeAddedToCart} />} />
 					<Route path="/cart" element={<Cart products_in_cart={cart_content} removeFunction={removeFromCart} />} />
