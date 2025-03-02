2127d6682dbc461cd0c4a3a25de8800194334f30
srysis <jashandenis@gmail.com>
2025-03-02 23:14:22 +0100

    added a 'total price' feature; slightly adjusted HTML markup

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -6,17 +6,29 @@ import "../style/cart/cart.scss"
 
 function Cart( {products_in_cart, removeFunction} ) {
 	if (products_in_cart.length > 0) {
+		let total_price = 0;
+
+		for (let item of products_in_cart) {
+			total_price += Number(item.price.replace(/[^0-9\.-]+/g,""));
+		}
+
 		return (
 			<div id="cart">
+				<div id="title"><h1>Your cart:</h1></div>
 				{products_in_cart.map((item) => {
 					return <CartProduct key={item.id} product={item} onRemove={removeFunction} />
 				})}
+				<hr />
+				<div id="total_price">
+					<div id="text"><h2>Total price:</h2></div>
+					<div id="price"><p>{`$${total_price}.00`}</p></div>
+				</div>
 			</div>
 		)
 	} else {
 		return (
 			<div id="cart">
-				<h2>Your cart is currently empty.</h2>
+				<h1>Your cart is currently empty.</h1>
 			</div>
 		)
 	}
