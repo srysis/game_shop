76901eb43a131ee28176cb4c9cdd3eb0bead7b79
srysis <jashandenis@gmail.com>
2025-03-05 16:31:18 +0100

    added a component that allows to toggle filters on/off

---
+++ src/components/filters/filters.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import "../../style/catalog/filters.scss"
+
+function Filters({setFiltersFunction, removeFilterFunction}) {
+	function onChangeHandler(checkbox, value) {
+		if (checkbox.checked) {
+			setFiltersFunction(value);
+		} else {
+			removeFilterFunction(value);
+		}
+	}
+
+	return(
+		<div id="filters">
+			<form>
+				<label htmlFor="stealth" className="checkbox_container">
+					Stealth
+					<input type="checkbox" name="tag_filter" id="stealth" value="Stealth" onChange={(event) => { onChangeHandler(event.target, event.target.value.toLowerCase()); } } />
+					<span className="checkmark"></span>
+				</label>
+				<label htmlFor="shooter" className="checkbox_container">
+					Shooter
+					<input type="checkbox" name="tag_filter" id="shooter" value="Shooter" onChange={(event) => { onChangeHandler(event.target, event.target.value.toLowerCase()); } } />
+					<span className="checkmark"></span>
+				</label>
+			</form>
+		</div>
+	)
+}
+
+export default Filters;
