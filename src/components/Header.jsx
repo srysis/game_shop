1b5a76844b4adf1641fa825c034866eb1ef600df
srysis <jashandenis@gmail.com>
2025-03-12 15:22:46 +0100

    reworked cart logic - now user is being shown a pop-up element with cart's content first; implemented all the neccessary logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,60 @@ import cart_icon from "../files/cart.png"
 import SearchBar from "./search/search_bar.jsx"
 import SearchResults from "./search/search_results.jsx"
 
+import CartOverlay from "./cart/cart_overlay.jsx"
+
 import "../style/layout/header.scss"
 
-function Header() {
+function Header({ cart_content }) {
 	const [search_queue, setSearchQueue] = React.useState("");
 	const [is_searching, setSearching] = React.useState(false);
+
+	const [is_cart_active, setCartActive] = React.useState(false);
 
 	function setSearchQueueFunction(queue) {
 		setSearchQueue(queue);
 	}
 
+	// to properly hide/show scrollbar, use 'setSearchingFunction(bool)' or 'setCartActiveFunction(bool)'
+	// to set state values
+
 	function setSearchingFunction(value) {
 		setSearching(value);
+
+		// hide scrollbar and adjust margin if fullscreen overlay is visible
+		if (value) { 
+			document.body.style.overflowX = 'hidden';
+			document.body.style.overflowY = 'hidden';
+			document.body.style.marginRight = 0.4 + "%";
+		} else { 
+			document.body.style.overflowX = 'visible';
+			document.body.style.overflowY = 'scroll';
+			document.body.style.marginRight = 0;
+		}
+	}
+
+	function setCartActiveFunction(value) {
+		setCartActive(value);
+
+		// hide scrollbar and adjust margin if fullscreen overlay is visible
+		if (value) { 
+			document.body.style.overflowX = 'hidden';
+			document.body.style.overflowY = 'hidden';
+			document.body.style.marginRight = 0.4 + "%";
+		} else { 
+			document.body.style.overflowX = 'visible';
+			document.body.style.overflowY = 'scroll';
+			document.body.style.marginRight = 0;
+		}
 	}
 
 	return (
 		<header>
 			{is_searching &&
-				<div id="search_overlay" onClick={() => setSearching(false)} ></div>
+				<div id="search_overlay" onClick={() => setSearchingFunction(false)} ></div>
+			}
+			{is_cart_active &&
+				<div id="cart_overlay" onClick={() => setCartActiveFunction(false)} ></div>
 			}
 			<div id="logo_container">
 				<h1 id="logo"><Link to="/">Game Shop</Link></h1>
@@ -34,8 +70,9 @@ function Header() {
 					<SearchResults setSearchingFunction={setSearchingFunction} search_queue={search_queue.toLowerCase()} />
 				}
 			</div>
-			<div id="cart_link_container">
-				<Link to="/cart" ><img src={cart_icon} alt="Go to cart" className="cart_icon" /></Link>
+			<div id="cart_container">
+				<button type="button" id="open_cart_overlay" onClick={() => {setCartActiveFunction(true)}}><img src={cart_icon} alt="Go to cart" className="cart_icon" /></button>
+				{is_cart_active && <CartOverlay cart_content={cart_content} setCartActiveFunction={setCartActiveFunction} />}
 			</div>
 		</header>
 	);
