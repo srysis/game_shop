a2787543e3a3da69b67377c100acfca7fc888c6f
srysis <jashandenis@gmail.com>
2025-03-21 15:56:21 +0100

    destructured 'product' object prop

--- src/components/cart/cart_overlay_product.jsx
+++ src/components/cart/cart_overlay_product.jsx
@@ -1,19 +1,22 @@
 import React from "react"
 import { Link } from "react-router";
 
-function CartOverlayProduct( {product, setCartActiveFunction} ) {
+function CartOverlayProduct({
+	  product: {id, box_art, name, price}, 
+	  setCartActiveFunction
+	}) {
 	return (
 		<div className="cart_overlay_product">
 			<div className="link_container">
 				<div className="overlay">
-					<Link to={`/product/${product.id}`} onClick={() => { setCartActiveFunction(false) }} />
+					<Link to={`/product/${id}`} onClick={() => { setCartActiveFunction(false) }} />
 				</div>
 				<div className="box_art">
-					<img src={`media/images/box_art/${product.box_art}`} alt={`${product.name} box art`} />
+					<img src={`media/images/box_art/${box_art}`} alt={`${name} box art`} />
 				</div>
 				<div className="product_info">
-					<h2>{product.name}</h2>
-					<span className="price"><p>{product.price}</p></span>
+					<h2>{name}</h2>
+					<span className="price"><p>{price}</p></span>
 				</div>
 			</div>
 		</div>
--- src/components/cart/cart_product.jsx
+++ src/components/cart/cart_product.jsx
@@ -3,22 +3,25 @@ import { Link } from "react-router";
 
 import remove_icon from "../../files/cross.svg"
 
-function CartProduct( {product, onRemove} ) {
+function CartProduct({
+	  product: {id, box_art, name, price}, 
+	  onRemove
+	}) {
 	return (
 		<div className="cart_product">
 			<div className="link_container">
 				<div className="overlay">
-					<Link to={`/product/${product.id}`} />
+					<Link to={`/product/${id}`} />
 				</div>
 				<div className="box_art">
-					<img src={`media/images/box_art/${product.box_art}`} alt={`${product.name} box art`} />
+					<img src={`media/images/box_art/${box_art}`} alt={`${name} box art`} />
 				</div>
 				<div className="product_info">
-					<h2><Link to={`/product/${product.id}`}>{product.name}</Link></h2>
-					<p>{product.price}</p>
+					<h2><Link to={`/product/${id}`}>{name}</Link></h2>
+					<p>{price}</p>
 				</div>
 				<div className="remove_from_cart_container">
-					<button type="button" className="remove_from_cart" onClick={ () => { onRemove(product.id) } }><img src={remove_icon} /></button>
+					<button type="button" className="remove_from_cart" onClick={ () => { onRemove(id) } }><img src={remove_icon} /></button>
 				</div>
 			</div>
 			
