69e1f4053a2219220b4ed4e3c8f902093f94cd0e
srysis <jashandenis@gmail.com>
2025-03-22 14:19:28 +0100

    added 'onClick' event for the root 'div' to remove the message on user's click

--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -5,9 +5,9 @@ import acknowledge_icon from "../files/confirm-icon.png"
 import "../style/layout/message.scss"
 import "../style/layout/mobile/message.scss"
 
-function SuccessMessage( {type} ) {
+function SuccessMessage( {type, onClickHandler} ) {
 	return (
-		<div className={type === "add" ? "message added" : type === "remove" ? "message removed" : ""} >
+		<div className={type === "add" ? "message added" : type === "remove" ? "message removed" : ""} onClick={() => {onClickHandler()}} >
 			<div className="icon_container">
 				<img src={acknowledge_icon} />
 			</div>
