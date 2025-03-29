13bf540a2d65178f91a7ed6769b386d438afe240
srysis <jashandenis@gmail.com>
2025-03-29 14:31:48 +0100

    modified HTML layout

--- src/components/cart/cart_overlay.jsx
+++ src/components/cart/cart_overlay.jsx
@@ -10,15 +10,17 @@ function CartOverlay( {cart_content, setCartActiveFunction} ) {
 
 	if (cart_content.length > 0) {
 		return (
-			<div id="cart_overlay_content">
+			<div id="cart_overlay_container">
 				<h1>Your cart</h1>
-				{cart_content.map((item, index) => <CartOverlayProduct key={item.id} product={item} setCartActiveFunction={setCartActiveFunction} />)}
+				<div id="cart_overlay_content">
+					{cart_content.map((item, index) => <CartOverlayProduct key={item.id} product={item} setCartActiveFunction={setCartActiveFunction} />)}
+				</div>
 				<div id="go_to_cart"><Link to="/cart" onClick={() => setCartActiveFunction(false)}>Manage cart</Link></div>
 			</div>
 		);
 	} else {
 		return (
-			<div id="cart_overlay_content">
+			<div id="cart_overlay_container">
 				<h1 className="empty_cart">Your cart is currently empty.</h1>
 			</div>
 		);
