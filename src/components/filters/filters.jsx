497feab97652c634f90f41a304d017675348c9b6
srysis <jashandenis@gmail.com>
2025-03-29 14:33:00 +0100

    renamed 'onClickHandler' to 'onClickHandlerMobile'; added 'onClick' event listener to every 'h3' tag in 'filters_container'

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -16,46 +16,51 @@ function Filters({device_type, setFiltersFunction, removeFilterFunction}) {
 		}
 	}
 
-	function onClickHandler() {
+	function onClickHandlerMobile() {
 		document.querySelector("form#filters_form").classList.toggle("active");
+	}
+
+	function onClickHandlerDesktop(event) {
+		event.currentTarget.classList.toggle("active");
+		event.currentTarget.nextElementSibling.classList.toggle("visible");
 	}
 
 	return(
 		<div id="filters">
 			{ device_type === "mobile" && 
 				<>
-					<button type="button" className="filters_toggler" onClick={() => onClickHandler()} >Filters</button>
+					<button type="button" className="filters_toggler" onClick={() => onClickHandlerMobile()} >Filters</button>
 					<hr />
 				</>
 			}
 			{ device_type === "desktop" && <h2>Filters</h2> }
 			<form id="filters_form">
 				<div className="filters_container">
-					<h3>Genre</h3>
+					<h3 onClick={onClickHandlerDesktop} >Genre</h3>
 					<div>
 						{Object.values(filters[0].genre).map((tag, index) => <Filter key={index + 1} filter={tag} onChangeHandler={onChangeHandler} /> )}
 					</div>
 				</div>
 				<div className="filters_container">
-					<h3>Player count</h3>
+					<h3 onClick={onClickHandlerDesktop} >Player count</h3>
 					<div>
 						{Object.values(filters[0].player_count).map((tag, index) => <Filter key={index + 1} filter={tag} onChangeHandler={onChangeHandler} /> )}
 					</div>
 				</div>
 				<div className="filters_container">
-					<h3>Format</h3>
+					<h3 onClick={onClickHandlerDesktop} >Format</h3>
 					<div>
 						{Object.values(filters[0].format).map((tag, index) => <Filter key={index + 1} filter={tag} onChangeHandler={onChangeHandler} /> )}
 					</div>
 				</div>
 				<div className="filters_container">
-					<h3>Setting</h3>
+					<h3 onClick={onClickHandlerDesktop} >Setting</h3>
 					<div>
 						{Object.values(filters[0].setting).map((tag, index) => <Filter key={index + 1} filter={tag} onChangeHandler={onChangeHandler} /> )}
 					</div>
 				</div>
 				<div className="filters_container">
-					<h3>Miscellaneous</h3>
+					<h3 onClick={onClickHandlerDesktop} >Miscellaneous</h3>
 					<div>
 						{Object.values(filters[0].misc_tags).map((tag, index) => <Filter key={index + 1} filter={tag} onChangeHandler={onChangeHandler} /> )}
 					</div>
