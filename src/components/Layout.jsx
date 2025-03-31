5708adedea91f62bbe72f16944ac600f819d3fb8
srysis <jashandenis@gmail.com>
2025-03-31 15:48:23 +0200

    added a new message type that can be shown

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,21 +5,25 @@ import { Outlet } from 'react-router-dom'
 import Header from "./Header.jsx"
 import SuccessAddMessage from "./SuccessMessage.jsx"
 import SuccessRemoveMessage from "./SuccessMessage.jsx"
+import SuccessClearMessage from "./SuccessMessage.jsx"
 
 
-function Layout( {device_type, was_added, was_removed, cart_content, resetMessages} ) {
+function Layout( {device_type, was_added, was_removed, was_cleared, cart_content, resetMessages} ) {
 	let message_type = "";
 
 	if (was_added) { 
 		message_type = "add";
 	} else if (was_removed) { 
 		message_type = "remove";
+	} else if (was_cleared) {
+		message_type = "clear";
 	}
 
 	return (
 		<>
 			{ was_added && <SuccessAddMessage type={message_type} onClickHandler={resetMessages} /> }
 			{ was_removed && <SuccessRemoveMessage type={message_type} onClickHandler={resetMessages} /> }
+			{ was_cleared && <SuccessClearMessage type={message_type} onClickHandler={resetMessages} /> }
 			<Header device_type={device_type} cart_content={cart_content} />
 			<main>
 				<Outlet />
--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -7,13 +7,20 @@ import "../style/layout/mobile/message.scss"
 
 function SuccessMessage( {type, onClickHandler} ) {
 	return (
-		<div className={type === "add" ? "message added" : type === "remove" ? "message removed" : ""} onClick={() => {onClickHandler()}} >
+		<div className="message" onClick={() => {onClickHandler()}} >
 			<div className="icon_container">
 				<img src={acknowledge_icon} />
 			</div>
 			<div className="text_container">
 				<h2>Success!</h2>
-				<p>{type === "add" ? "Product was added to your cart" : type === "remove" ? "Product was removed from your cart" : undefined}</p>
+				<p>
+				{
+					type === "add" ? "Product was added to your cart" 
+				  : type === "remove" ? "Product was removed from your cart" 
+				  : type === "clear" ? "Cart was successfully cleared." 
+				  : undefined
+				}
+				</p>
 			</div>
 		</div>
 	);
